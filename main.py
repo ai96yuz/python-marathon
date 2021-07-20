371e3c8e284a38a64cd8964ed06b33b887b061c6
ai96yuz <ai96yuz@gmail.com>
2021-07-20 10:50:58 +0300

    updatet main.py / added venv

--- main.py
+++ main.py
@@ -7,7 +7,8 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-g
+    print('\n Test')
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
