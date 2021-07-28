e2a63652efa43ef620b441e9cbf16752015b9a29
ai96yuz <ai96yuz@gmail.com>
2021-07-29 01:09:54 +0300

    added 1st task from sprint03

--- sprint03/task.py
+++ sprint03/task.py
@@ -1,13 +1,23 @@
 # Task 1
 
 
-
+def outer(name):
+    def inner():
+        print('Hello, {}!'.format(name))
+    return inner
 
 
 # Task 2
 
 
-
+# def outer(data):
+#     def inner():
+#         firstValue = data
+#         return firstValue
+#     lambda x: x if data == inner else None
+#
+#
+# print(firstValue("secret"))
 
 
 # Task 3
