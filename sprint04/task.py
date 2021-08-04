059c45f6d4264377c14dae2ed15a02ea18e5625a
ai96yuz <ai96yuz@gmail.com>
2021-08-04 17:04:45 +0300

    Finished all tasks to sprint 4

--- sprint04/task.py
+++ sprint04/task.py
@@ -41,3 +41,71 @@ class Pizza(object):
 # Task 3
 
 
+class Employee:
+    def __init__(self, fullname, **kwargs):
+        self.name, self.lastname = fullname.split(' ')
+
+        for name, value in kwargs.items():
+            setattr(self, name, value)
+
+
+# Task 4
+
+
+class Testpaper:
+    def __init__(self, subject, markscheme, pass_mark):
+        self.subject = subject
+        self.markscheme = markscheme
+        self.pass_mark = pass_mark
+        self.passmark = int(pass_mark[0: -1])
+
+
+class Student:
+    def __init__(self):
+        self.tests_taken = 'No tests taken'
+        self.total_result = {}
+
+    def take_test(self, Testpaper, answers):
+        i = 0
+        correct_answer = 0
+        total_marks = 0
+        test_result = ''
+
+        for answer in Testpaper.markscheme:
+            if answer == answers[i]:
+                correct_answer += 1
+            i += 1
+        total_marks = round((correct_answer / i) * 100)
+
+        if total_marks >= Testpaper.passmark:
+            test_result = 'Passed!'
+        else:
+            test_result = 'Failed!'
+
+        self.total_result[Testpaper.subject] = test_result + ' (' + str(total_marks) + '%)'
+        self.tests_taken = self.total_result
+
+
+# Task 5
+
+
+class Gallows():
+    def __init__(self, words=[], game_over=False):
+        self.words = words
+        self.game_over = game_over
+
+    def play(self, word):
+        if not self.words:
+            self.words.append(word)
+            return self.words
+
+        if word not in self.words and self.words[-1][-1] == word[0]:
+            self.words.append(word)
+            return self.words
+        self.game_over = True
+        return 'game over'
+
+    def restart(self):
+        self.words = []
+        self.game_over = False
+        return 'game restarted'
