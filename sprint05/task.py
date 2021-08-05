be13c16d7d39e20abcab5b64f36c4b943924be6b
ai96yuz <ai96yuz@gmail.com>
2021-08-05 13:27:01 +0300

    Completed 1, 3, 4 task from 5 sprint

---
+++ sprint05/task.py
@@ -0,0 +1,58 @@
+# Task 1
+
+
+class ToSmallNumberGroupError(Exception):
+    def __init__(self, num):
+        self.num = num
+
+    def __str__(self):
+        return repr(self.num)
+
+
+def check_number_group(number):
+    try:
+        if isinstance(number, str):
+            number = int(number)
+
+        if number > 10:
+            return 'Number of your group {} is valid'.format(number)
+
+        if -10 < number <= 10:
+            raise ToSmallNumberGroupError('Number of your group can\'t be less than 10')
+    except ToSmallNumberGroupError as e:
+        return 'We obtain error:' + e.num
+    except:
+        return 'You entered incorrect data. Please try again.'
+
+
+# Task 2
+
+
+# Task 3
+
+import re
+
+
+def valid_email(txt):
+    validator = bool(re.search(r'^([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,})', txt))
+
+    try:
+        if not validator:
+            return 'Email is not valid'
+        if validator:
+            return 'Email is valid'
+
+    except:
+        return 'Email is not valid'
+
+
+# Task 4
+
+def check_odd_even(num):
+    try:
+        if (num % 2) == 0:
+            return 'Entered number is even'
+        else:
+            return 'Entered number is odd'
+    except:
+        return 'You entered not a number.'
