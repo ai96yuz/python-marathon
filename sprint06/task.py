3780b57278a2c14ddd155849629b9a89f05f42ba
ai96yuz <ai96yuz@gmail.com>
2021-08-10 21:04:16 +0300

    Completed task 2

--- sprint06/task.py
+++ sprint06/task.py
@@ -1,4 +1,6 @@
 # Task 1
+
+
 import json
 
 
@@ -22,3 +24,50 @@ def find(file, key):
 # Task 2
 
 
+import json
+import logging
+
+logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger('root')
+logger.setLevel(logging.ERROR)
+
+
+def parse_user(output_file, *input_files):
+    all_data = []
+    for file_name in input_files:
+        try:
+            with open(file_name, 'r') as f:
+                all_data.extend(json.load(f))
+        except FileNotFoundError:
+            logger.error(f'File {file_name} doesn\'t exists')
+    if all_data:
+        all_data.pop(-1)
+    with open(output_file, 'w') as outfile:
+        json.dump(all_data, outfile)
+    return output_file
+
+
+def print_file(output_file):
+    with open(output_file, 'r') as f:
+        if output_file == 'app.log':
+            print(f.read())
+        else:
+            json_file = json.load(f)
+            print(json.dumps(json_file, indent = 4))
+
+
+# Task 3
+
+
+import json
+import jsonschema
+from jsonschema import validate
+import csv
+
+
+def user_with_department(csv_file, user_json, department_json):
+    pass
+
+
+def validate_json(data, schema):
+    pass
