b8a847f1aa51460c64733f79da3f7258820d6f3e
ai96yuz <ai96yuz@gmail.com>
2021-08-11 22:27:20 +0300

    Completed task 4

--- sprint06/task.py
+++ sprint06/task.py
@@ -56,18 +56,50 @@ def print_file(output_file):
             print(json.dumps(json_file, indent = 4))
 
 
-# Task 3
+# Task 4
 
 
 import json
-import jsonschema
-from jsonschema import validate
-import csv
 
 
-def user_with_department(csv_file, user_json, department_json):
-    pass
+class Student:
+    def __init__(self, full_name: str, avg_rank: float, courses: list):
+        self.full_name = full_name
+        self.avg_rank = avg_rank
+        self.courses = courses
+
+    def __str__(self):
+        return '{} ({}): {}'.format(self.full_name, self.avg_rank, self.courses)
+
+    @classmethod
+    def from_json(cls, json_file):
+        with open(json_file) as f:
+            student_data = json.load(f)
+        return '{} ({}): {}'.format(student_data.get('full_name'), student_data.get('avg_rank'), student_data.get('courses'))
 
 
-def validate_json(data, schema):
-    pass
+class Group:
+    def __init__(self, title: str, students: list):
+        self.title = title
+        self.students = students
+
+    def __str__(self):
+        students = [str(Student(**item)) for item in self.students]
+        return f'{self.title}: {students}'
+
+    @classmethod
+    def create_group_from_file(cls, students_file):
+        with open(students_file) as f:
+            stud_data = json.load(f)
+        if not isinstance(stud_data, list):
+            stud_data = [stud_data]
+        return cls(students_file.split('.')[0], stud_data)
+
+    @classmethod
+    def serialize_to_json(cls, list_of_groups, filename):
+        result_list = []
+        for el in list_of_groups:
+            result_dict = {'title': el.title, 'students': el.students}
+            result_list.append(result_dict)
+        with open(filename, 'a') as f:
+            json.dump(result_list, f)
