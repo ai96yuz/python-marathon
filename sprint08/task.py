f2c3a9950ce7166f74ddb85ed1eefe09c58b1733
ai96yuz <ai96yuz@gmail.com>
2021-08-19 16:00:45 +0300

    Added some changes to 8 sprint

--- sprint08/task.py
+++ sprint08/task.py
@@ -91,7 +91,7 @@ def quadratic_equation(a, b, c):
         elif discriminant > 0:
             x1 = (-b + math.sqrt(discriminant)) / (2 * a)
             x2 = (-b - math.sqrt(discriminant)) / (2 * a)
-            return (x1, x2)
+            return x1, x2
 
 
 class QuadraticEquationTest(unittest.TestCase):
